a67e2f6e8e72d6bdb0cffc060d05db88057699a0
Matthew <matty.beckett@btinternet.com>
2013-08-10 10:07:47 +0100

    Update video.sh

--- video.sh
+++ video.sh
@@ -2,7 +2,7 @@
  
 while [ 1 ]; do # do forever
 {
-raspivid -w 1920 -h 1080 -n -o /home/pi/`date +%H%M%S` -t 300000 # executing recording with some parameters
+raspivid -w 1280 -h 720 -n -o /home/pi/`date +%H%M%S` -t 300000 # executing recording with some parameters
 sleep 1 # delay for one second
 }
  
