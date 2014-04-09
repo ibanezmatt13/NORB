a41a2acac2b37841b36222c183d6736173101c91
Matthew <matty.beckett@btinternet.com>
2014-04-09 18:57:06 +0100

    Update NORB.sh

--- NORB.sh
+++ NORB.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-while [ 1 ]; do # do  forever
+while [ 1 ] do # do  forever
 {
-/home/pi/NORB.py; # execute the main tracker executable
+/home/pi/NORB.py & # execute the main tracker executable
 sleep 1 # delay for one second
 }
 
